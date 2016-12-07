6d29bbcf6965e066ef11c91fe3460bbb7256d911
Bhurinat Wangsutthitham <bhurinat.wangsutthitham@thomsonreuters.com>
2016-12-07 16:33:36 +0700

    Add dumpVersion

--- tasks/version.js
+++ tasks/version.js
@@ -1,15 +1,12 @@
 'use strict';
 
-var gulp = require('gulp');
 var fs = require('fs');
 
 const versionFile = 'version.txt';
 
-gulp.task('version', function () {
-    let pkg = (JSON.parse(fs.readFileSync('./package.json')));
-    let version = pkg.version;
+let pkg = (JSON.parse(fs.readFileSync('./package.json')));
+let version = pkg.version;
 
-    fs.writeFile(versionFile, "version="+version, (err) => {
-        if (err) throw err;
-    });
+fs.writeFile(versionFile, "version="+version, (err) => {
+    if (err) throw err;
 });
