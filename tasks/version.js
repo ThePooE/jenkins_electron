010ba6bbca3740161166da1bf7d9994c91d273b4
Bhurinat Wangsutthitham <bhurinat.wangsutthitham@thomsonreuters.com>
2016-12-07 13:27:59 +0700

    Export version from package.json to version.txt

--- tasks/version.js
+++ tasks/version.js
@@ -3,7 +3,13 @@
 var gulp = require('gulp');
 var fs = require('fs');
 
+const versionFile = 'version.txt';
+
 gulp.task('version', function () {
     let pkg = (JSON.parse(fs.readFileSync('./package.json')));
-    let version = console.log(pkg.version);
+    let version = pkg.version;
+
+    fs.writeFile(versionFile, "version=" + version, (err) => {
+        if (err) throw err;
+    });
 });
